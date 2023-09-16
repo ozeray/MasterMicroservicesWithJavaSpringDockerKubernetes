a9a26b79edbb60eb27c5be49a07fe16e2791601a
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2023-09-16 22:53:04 +0300

    ...

--- gatewayserver/src/main/java/com/ahmet/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/ahmet/gatewayserver/GatewayserverApplication.java
@@ -8,8 +8,6 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
-
 @SpringBootApplication
 public class GatewayserverApplication {
 
